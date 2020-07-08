2252c249609a0778e54ace01a45b0b5c2f2fb80d
qiu <qdaolong@126.com>
2020-07-08 15:26:35 -0400

    Solved 2 problems 1)HeightChecker 2)SortArrayByParity

---
+++ src/test/com/leetcode/array/inplace/SortArrayByParityTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.array.inplace;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortArrayByParityTest {
+
+    @Test
+    public void sortArrayByParity() {
+        int[] input = new int[]{3,1,2,4};
+        int[] expect = new int[]{2,4,3,1};
+        assertArrayEquals(expect, SortArrayByParity.sortArrayByParity(input));
+    }
+
+    @Test
+    public void sortArrayByParityTwoPointers() {
+        int[] input = new int[]{3,1,2,4};
+        int[] expect = new int[]{4,2,1,3};
+        assertArrayEquals(expect, SortArrayByParity.sortArrayByParityTwoPinter(input));
+    }
+
+
+}
